dbfb3e32e2906c6bba69c4ccf5ded6975fda5b58
sjfremen <129542120+sjfremen@users.noreply.github.com>
2023-04-06 10:02:25 -0400

    Update dashtest.py

--- dashtest.py
+++ dashtest.py
@@ -61,6 +61,36 @@ fig3.update_layout(width=1200, height=600)
 title_text = "BTC Price Versus Key Price Levels"
 fig3.update_layout(title=title_text)
 
+fig3.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    )
+)
+
 #Dash Example
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
  
