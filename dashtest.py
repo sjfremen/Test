c4caecc4fd283cd1f801161009df2ab3585aa4d6
sjfremen <129542120+sjfremen@users.noreply.github.com>
2023-04-13 10:40:11 -0400

    Update dashtest.py

--- dashtest.py
+++ dashtest.py
@@ -63,36 +63,6 @@ fig3.add_trace(go.Scatter(x=df['t'], y=df['200wavg'], name = '200w avg'))
 title_text = "BTC Price Versus Key Price Levels"
 fig3.update_layout(title=title_text)
 
-fig3.update_layout(
-    xaxis=dict(
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1,
-                     label="1m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=6,
-                     label="6m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=1,
-                     label="YTD",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=1,
-                     label="1y",
-                     step="year",
-                     stepmode="backward"),
-                dict(step="all")
-            ])
-        ),
-        rangeslider=dict(
-            visible=True
-        ),
-        type="date"
-    )
-)
-
 fig3.update_yaxes(fixedrange=False)
 
 #Dash Example
