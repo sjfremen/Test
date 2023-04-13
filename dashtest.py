e7dc80af05850a81609bc0e9661c147e1c4d47bd
sjfremen <129542120+sjfremen@users.noreply.github.com>
2023-04-13 12:23:35 -0400

    Update dashtest.py

--- dashtest.py
+++ dashtest.py
@@ -3,28 +3,35 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 import plotly.express as px
+import plotly.offline as pyo
 import pandas as pd
 from PIL import Image # new import
 
 # Create Chart And Percentile Metrics
 df = pd.read_csv('metrics.csv')
+df = df[(df['t'] > "2012-01-01")]
 
 df['test'] = df.rhodl_ratio.rank(pct = True)
 df['test2'] = df.mvrv_z_score.rank(pct = True)
 df['200davg'] = df.price_usd_close.rolling(window=200).mean()
 df['200wavg'] = df.price_usd_close.rolling(window=1400).mean()
-
-fig = px.scatter(df, x='t', y='price_usd_close', color='test', 
-                 log_y=True, title="BTC Price Weighted By RHODL Percentile",
-                 width=800, height=400)
-
-fig2 = px.scatter(df, x='t', y='price_usd_close', color='test2', 
-                 log_y=True, title="BTC Price Weighted By MVRV Z Percentile",
-                 width=800, height=400)
+df['rhodl_percentile'] = df.rhodl_ratio.rank(pct = True)
+df['mvrv_z_percentile'] = df.mvrv_z_score.rank(pct = True)
+df['reserve_risk_percentile'] = df.reserve_risk.rank(pct = True)
+df['puell_multiple_percentile'] = df.puell_multiple.rank(pct = True)
+df['nupl_percentile'] = df.net_unrealized_profit_loss.rank(pct = True)
 
 img = Image.open('LOGOsmall.png') # image path
 
-layout = go.Layout(
+df2 = df[['t', 'rhodl_percentile', 'mvrv_z_percentile', 'reserve_risk_percentile','puell_multiple_percentile', 'nupl_percentile']]
+
+# Define the heatmap trace
+trace = go.Heatmap(x=df2['t'], y=df2.columns[1:], z=df2.iloc[:, 1:].values.T, colorscale='RdYlGn', dy=10, reversescale=True)
+
+# Define the layout
+heatlayout = go.Layout(title='BTC On-Chain Cycle Metrics Heatmap by Percentile')
+
+pricelayout = go.Layout(
     title="Data",
     plot_bgcolor="#FFF",  # Sets background color to white
     xaxis=dict(
@@ -41,8 +48,13 @@ layout = go.Layout(
 
 fig3 = go.Figure(
     data=go.Scatter(x=df['t'],y=df['price_usd_close'], name = 'price'),
-    layout=layout
+    layout=pricelayout
 ) 
+
+# Create the figure object and plot it
+fig = go.Figure(data=[trace], layout=heatlayout)
+##fig.layout.height = 800
+#pyo.plot(fig, filename='heatmap.html')
 
 # defined to source
 fig3.add_layout_image(
@@ -80,9 +92,12 @@ app.layout = html.Div(className='row', children=[
     html.H1("BM PRO Dashboard MVP"),
     html.Div(children=[
         dcc.Graph(id="graph1", figure=fig3, style={'display': 'inline-block'}),
-        dcc.Graph(id="graph2", figure=fig3, style={'display': 'inline-block'}),
+        dcc.Graph(id="graph2", figure=fig, style={'display': 'inline-block'})
+        
+     ]),
+    html.Div(children=[
         dcc.Graph(id="graph3", figure=fig3, style={'display': 'inline-block'}),
-        dcc.Graph(id="graph4", figure=fig3, style={'display': 'inline-block'})
+        dcc.Graph(id="graph4", figure=fig, style={'display': 'inline-block'})
     ])
 ])
 
